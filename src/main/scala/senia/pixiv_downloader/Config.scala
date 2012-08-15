00c3fa5aa60dce52fc7cb5b5e45855e89f4db71d
senia <seniapsm@gmail.com>
2012-08-15 23:00:34 +0400

    command line arguments

--- src/main/scala/senia/pixiv_downloader/Config.scala
+++ src/main/scala/senia/pixiv_downloader/Config.scala
@@ -1,3 +1,75 @@
 package senia.pixiv_downloader
 
-case class Config(login: String, password: String, helpRequest: Boolean = false, proxy: Option[Proxy] = None)
+import scalaz.Scalaz._
+
+case class Config(login: String, password: String, proxy: Option[Proxy] = None)
+
+case class ParameterKey(key: String, name: String, symbol: Symbol, withParameter: Boolean, description: Option[String] = None)
+case class MapMatcher[K, V](map: Map[K, V]) {
+  def unapply(k: K): Option[V] = map.get(k)
+}
+
+object Config{
+  val mandatoryKeys = Set(
+    ParameterKey("-l", "pixiv login", 'login, withParameter = true),
+    ParameterKey("--login", "pixiv login", 'login, withParameter = true),
+    ParameterKey("-p", "pixiv password", 'password, withParameter = true),
+    ParameterKey("--password", "pixiv password", 'password, withParameter = true)
+  )
+
+  val optionalKeys = Set(
+    ParameterKey("--proxy", "proxy", 'proxy, withParameter = true, "--proxy [protocol://][login:password@]proxy_name[:port]".some),
+    ParameterKey("-h", "help", 'help, withParameter = false),
+    ParameterKey("--help", "help", 'help, withParameter = false)
+  )
+
+  private def mkDescription(keys: Set[ParameterKey]) =
+    keys.
+      groupBy(k => k.symbol).
+      map {case (s, pks) =>
+      pks.head.description.getOrElse(
+        (pks.map{_.key}.toList match {
+          case head :: Nil => head
+          case x => x.mkString("(", " | ", ")")
+        }) +
+          (if (pks.head.withParameter) " " + pks.head.name.replace(' ', '_') else "")
+      )
+    }
+
+  val usage =
+    "pixiv_downloader " +
+      mkDescription(mandatoryKeys).mkString(" ") +
+      " " +
+      mkDescription(optionalKeys).map{ "[" + _ + "]" }.mkString(" ")
+
+  def parseArguments[T <% Seq[String]](args: T): ValidationNEL[String, Map[Symbol, Option[String]]] = {
+    @annotation.tailrec def parseParams(args: List[String],
+                                        keys: Map[String, ParameterKey],
+                                        subresult: ValidationNEL[String, Map[Symbol, Option[String]]] = Map().success
+                                         ): ValidationNEL[String, Map[Symbol, Option[String]]] = {
+      val withParameter = MapMatcher(keys.filter{ case (k, v) => v.withParameter })
+      val withoutParameter = MapMatcher(keys.filter{ case (k, v) => !v.withParameter })
+      args match {
+        case Nil => subresult
+        case `withParameter`(k) :: l :: tail => parseParams(tail, keys, subresult.map { _ + (k.symbol -> l.some) })
+        case `withParameter`(k) :: Nil =>
+          (subresult |@| ("You should specify " + k.name + " after " + k.key + " key").failNel[Map[Symbol, String]])((c, r) => c)
+        case `withoutParameter`(k) :: tail => parseParams(tail, keys, subresult.map { _ + (k.symbol -> None) })
+        case head :: tail => parseParams(tail, keys, (subresult |@| ("Unsupported parameter: \"" + head + "\"").failNel[Config])((c, p) => c))
+      }
+    }
+
+    parseParams(args.toList, (mandatoryKeys ++ optionalKeys).map{ k => k.key -> k}(collection.breakOut))
+  }
+
+  def apply(params: Map[Symbol, Option[String]]): ValidationNEL[String, Config] = {
+    def getMandatory(keys: Map[Symbol, Option[String]], key: Symbol): ValidationNEL[String, String] =
+      keys.get(key).flatMap( identity ).map{ _.successNel }.getOrElse( (key.toString().drop(1) + " is mandatory").failNel )
+
+    for {
+      l <- getMandatory(params, 'login)
+      p <- getMandatory(params, 'password)
+      proxy <- params.get('proxy).flatMap(identity).map{Proxy.parse(_).map{_.some}}.getOrElse(None.successNel)
+    } yield Config(l, p, proxy)
+  }
+}
--- src/main/scala/senia/pixiv_downloader/Test.scala
+++ src/main/scala/senia/pixiv_downloader/Test.scala
@@ -4,92 +4,25 @@ import pixiv.{Url, Client}
 import scalaz._
 import Scalaz._
 
-case class ParameterKey(key: String, name: String, symbol: Symbol, withParameter: Boolean, description: Option[String] = None)
-case class MapMatcher[K, V](map: Map[K, V]) {
-  def unapply(k: K): Option[V] = map.get(k)
-}
-
 object Test extends App {
-  @annotation.tailrec def parseParams(args: List[String],
-                                      keys: Map[String, ParameterKey],
-                                      subresult: ValidationNEL[String, Map[Symbol, Option[String]]] = Map().success
-                                     ): ValidationNEL[String, Map[Symbol, Option[String]]] = {
-    val withParameter = MapMatcher(keys.filter{ case (k, v) => v.withParameter })
-    val withoutParameter = MapMatcher(keys.filter{ case (k, v) => !v.withParameter })
-    args match {
-      case Nil => subresult
-      case `withParameter`(k) :: l :: tail => parseParams(tail, keys, subresult.map { _ + (k.symbol -> l.some) })
-      case `withParameter`(k) :: Nil =>
-        (subresult |@| ("You should specify " + k.name + " after " + k.key + " key").failNel[Map[Symbol, String]])((c, r) => c)
-      case `withoutParameter`(k) :: tail => parseParams(tail, keys, subresult.map { _ + (k.symbol -> None) })
-      case head :: tail => parseParams(tail, keys, (subresult |@| ("Unsupported parameter: \"" + head + "\"").failNel[Config])((c, p) => c))
-    }
+  def showUsageAndExit(errors: Option[NonEmptyList[String]] = None) = {
+    errors.foreach(l => Console.err.println(l.list.mkString("\n")))
+    println("Usage:")
+    println(Config.usage)
+    sys.exit(1)
   }
 
-  val mandatoryKeys = Set(
-    ParameterKey("-l", "pixiv login", 'login, withParameter = true),
-    ParameterKey("--login", "pixiv login", 'login, withParameter = true),
-    ParameterKey("-p", "pixiv password", 'password, withParameter = true),
-    ParameterKey("--password", "pixiv password", 'password, withParameter = true)
-  )
-
-  val optionalKeys = Set(
-    ParameterKey("--proxy", "proxy", 'proxy, withParameter = true, "--proxy [protocol://][login:password@]proxy_name[:port]".some),
-    ParameterKey("-h", "help", 'help, withParameter = false),
-    ParameterKey("--help", "help", 'help, withParameter = false)
-  )
-
-  def mkDescription(keys: Set[ParameterKey]) =
-    keys.
-      groupBy(k => k.symbol).
-      map {case (s, pks) =>
-        pks.head.description.getOrElse(
-          (pks.map{_.key}.toList match {
-            case head :: Nil => head
-            case x => x.mkString("(", " | ", ")")
-          }) +
-          (if (pks.head.withParameter) " " + pks.head.name.replace(' ', '_') else "")
-        )
-      }
-
-  val usage =
-    "pixiv_downloader " +
-      mkDescription(mandatoryKeys).mkString(" ") +
-      " " +
-      mkDescription(optionalKeys).map{ "[" + _ + "]" }.mkString(" ")
-
-  val configMap = parseParams(args.toList, (mandatoryKeys ++ optionalKeys).map{ k => k.key -> k}(collection.breakOut)) match {
-    case Failure(f) =>
-      println(f.list.mkString("\n"))
-      println("Usage:")
-      println(usage)
-      sys.exit(1)
+  val configMap = Config.parseArguments(args) match {
+    case Failure(f) => showUsageAndExit(f.some)
     case Success(c) => c
   }
 
   if (configMap.get('help).map{ _ => true }.getOrElse(false)) {
-    println("Usage:")
-    println(usage)
-    sys.exit(0)
+    showUsageAndExit()
   }
 
-  def getMandatory(keys: Map[Symbol, Option[String]], key: Symbol): ValidationNEL[String, String] =
-    keys.get(key).flatMap( identity ).map{ _.successNel }.getOrElse( (key.toString().drop(1) + " is mandatory").failNel )
-
-  val configResult =
-    for {
-      l <- getMandatory(configMap, 'login)
-      p <- getMandatory(configMap, 'password)
-      proxy <- configMap.get('proxy).flatMap(identity).map{Proxy.parse(_).map{_.some}}.getOrElse(None.successNel)
-      h <- configMap.get('help).map{ _ => true }.getOrElse(false).successNel
-    } yield Config(l, p, h, proxy)
-
-  val config = configResult match {
-    case Failure(f) =>
-      println(f.list.mkString("\n"))
-      println("Usage:")
-      println(usage)
-      sys.exit(1)
+  val config = Config(configMap) match {
+    case Failure(f) => showUsageAndExit(f.some)
     case Success(c) => c
   }
 
